9ffbe5f7a63bf8b9cf3b2afdf8e02341ab88e775
jim90247 <jim90247@gmail.com>
2021-05-03 00:36:54 +0800

    Update logging steps in wrapper

--- run_ns_wrapper.py
+++ run_ns_wrapper.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def main(args):
     param_dict = {
-        'model_name_or_path': args.model_path if args.model_name is None else args.model_name,
+        'model_name_or_path': args.model,
         'do_train': True,
         'do_eval': True,
         'do_predict': True,
@@ -20,10 +20,10 @@ def main(args):
         'qa_file': args.output / 'predictions.json',
         'overwrite_output_dir': args.overwrite,
         'logging_dir': args.output / 'tensorboard_output',
-        # 'logging_strategy': 'step',
-        # 'logging_steps': 500,
-        # 'evaluation_strategy': 'step',  # do_eval is True if evaluation_strategy is not 'no'
-        # 'eval_steps': 500,
+        'logging_strategy': 'steps',
+        'logging_steps': 500,
+        'evaluation_strategy': 'steps',  # do_eval is True if evaluation_strategy is not 'no'
+        'eval_steps': 500,
         'save_total_limit': 3,
         'per_device_train_batch_size': 16,
         'per_device_eval_batch_size': 384,
@@ -39,8 +39,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--model_name", help="Model name. Should not be used together with model_path.")
-    parser.add_argument("--model_path", help="Model path. Should not be used together with model_name.")
+    parser.add_argument("model", help="Name of the model (download from huggingface) or path to local model directory.")
     parser.add_argument("--dataset_root", type=Path, default=Path('dataset'))
     parser.add_argument("-o", "--output", help="output directory", required=True, type=Path)
     parser.add_argument(
@@ -49,11 +48,5 @@ if __name__ == '__main__':
     parser.add_argument("--lr", type=float, default=3e-5)
     parser.add_argument("-n", "--epoch", type=int, default=2)
     args = parser.parse_args()
-
-    # Check if argument combination is valid
-    if args.model_name is None and args.model_path is None:
-        raise ValueError("Must specify one of model_name or model_path")
-    elif args.model_name is not None and args.model_path is not None:
-        raise ValueError("model_name and model_path cannot be specified at the same time")
 
     main(args)
--- run_qa_wrapper.py
+++ run_qa_wrapper.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def main(args):
     param_dict = {
-        'model_name_or_path': args.model_path if args.model_name is None else args.model_name,
+        'model_name_or_path': args.model,
         'do_train': True,
         'do_eval': True,
         'do_predict': True,
@@ -14,13 +14,13 @@ def main(args):
         'output_dir': args.output,
         'overwrite_output_dir': args.overwrite,
         'logging_dir': args.output / 'tensorboard_output',
-        # 'logging_strategy': 'step',
-        # 'logging_steps': 500,
-        # 'evaluation_strategy': 'step',  # do_eval is True if evaluation_strategy is not 'no'
-        # 'eval_steps': 500,
+        'logging_strategy': 'steps',
+        'logging_steps': 300,
+        'evaluation_strategy': 'steps',  # do_eval is True if evaluation_strategy is not 'no'
+        'eval_steps': 300,
         'save_total_limit': 3,
         'per_device_train_batch_size': 16,
-        'per_device_eval_batch_size': 384,
+        'per_device_eval_batch_size': 128,
         'learning_rate': args.lr,
         'num_train_epochs': args.epoch,
         'max_seq_length': 384,
@@ -33,22 +33,15 @@ def main(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--model_name", help="Model name. Should not be used together with model_path.")
-    parser.add_argument("--model_path", help="Model path. Should not be used together with model_name.")
+    parser.add_argument("model", help="Name of the model (download from huggingface) or path to local model directory.")
     parser.add_argument("--dataset_root", type=Path, default=Path('dataset'))
+    parser.add_argument("--ns_prediction", type=Path, help="Path to next_sentence prediction output.", required=True)
     parser.add_argument("-o", "--output", help="output directory", required=True, type=Path)
     parser.add_argument(
         "--overwrite", help="Overwrite output directory contents when it exists.", action='store_true', default=False
     )
     parser.add_argument("--lr", type=float, default=3e-5)
     parser.add_argument("-n", "--epoch", type=int, default=2)
-    parser.add_argument("--ns_prediction", type=Path, help="Path to next_sentence prediction output.", required=True)
     args = parser.parse_args()
-
-    # Check if argument combination is valid
-    if args.model_name is None and args.model_path is None:
-        raise ValueError("Must specify one of model_name or model_path")
-    elif args.model_name is not None and args.model_path is not None:
-        raise ValueError("model_name and model_path cannot be specified at the same time")
 
     main(args)
