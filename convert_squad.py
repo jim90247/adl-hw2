072aafc8ed92c64ad9910540908efe125f3c5bc0
jim90247 <jim90247@gmail.com>
2021-05-05 00:01:24 +0800

    Add option to include no answer questions in QA

--- convert_squad.py
+++ convert_squad.py
@@ -7,7 +7,7 @@ import tqdm
 import random
 
 
-def hw2_to_squad(dataset: List[Dict], contexts: List[str]) -> List[Dict]:
+def hw2_to_squad(dataset: List[Dict], contexts: List[str], no_answer_frac: float) -> List[Dict]:
     """Convert homework 2 dataset format to SQuAD 1.0 format.
 
     Args:
@@ -32,26 +32,30 @@ def hw2_to_squad(dataset: List[Dict], contexts: List[str]) -> List[Dict]:
 
     for entry in tqdm.tqdm(dataset, desc="Converting dataset to SQuAD 1.0 format"):
         for field_name in required_fields:
-            if entry.get(field_name) is None:
+            if field_name not in entry:
                 raise ValueError("No {} in input dataset!".format(field_name))
 
-        squad_entry = {
-            'id': entry['id'],
-            'question': entry['question'],
-            'context': contexts[entry['relevant']],
-            'answers': {
-                'answer_start': [],
-                'text': []
+        no_answer_count = int(no_answer_frac / (1 - no_answer_frac))
+        paragraphs = random.sample([p for p in entry['paragraphs'] if p != entry['relevant']],
+                                   k=min(no_answer_count, len(entry['paragraphs'])-1)) + [entry['relevant']]
+        for context_id in paragraphs:
+            squad_entry = {
+                'id': entry['id'],
+                'question': entry['question'],
+                'context': contexts[context_id],
+                # Answers will be left empty for non-relevant paragraphs.
+                'answers': {
+                    'answer_start': [],
+                    'text': []
+                }
             }
-        }
 
-        if 'answers' in entry:
-            squad_entry['answers'] = defaultdict(list)
-            for answer in entry['answers']:
-                squad_entry['answers']['answer_start'].append(answer['start'])
-                squad_entry['answers']['text'].append(answer['text'])
+            if 'answers' in entry and context_id == entry['relevant']:
+                for answer in entry['answers']:
+                    squad_entry['answers']['answer_start'].append(answer['start'])
+                    squad_entry['answers']['text'].append(answer['text'])
 
-        squad_dataset.append(squad_entry)
+            squad_dataset.append(squad_entry)
 
     return squad_dataset
 
@@ -64,8 +68,8 @@ def main(args):
 
     if args.count is not None:
         dataset = random.sample(dataset, k=args.count)
-    
-    squad_dataset = hw2_to_squad(dataset, contexts)
+
+    squad_dataset = hw2_to_squad(dataset, contexts, args.no_answer_frac)
 
     with open(args.output, 'w', encoding='utf-8') as f:
         json.dump({"data": squad_dataset}, f, ensure_ascii=False, indent=2)
@@ -77,5 +81,6 @@ if __name__ == '__main__':
     parser.add_argument('contexts')
     parser.add_argument('output')
     parser.add_argument('--count', type=int)
+    parser.add_argument('--no_answer_frac', type=float, default=0.0)
     args = parser.parse_args()
     main(args)
